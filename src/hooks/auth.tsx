5111572851142c6e08b500d6b5bb61bc161f89aa
Rafael Alcindo <rafael_alcindo@hotmail.com>
2021-07-06 17:59:25 -0300

    começando autenticação no discord

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -6,6 +6,17 @@ import React,
     ReactNode
 } from 'react';
 
+import * as AuthSession from 'expo-auth-session';
+
+import {
+    SCOPE,
+    CLIENT_ID,
+    CDN_IMAGE,
+    REDIRECT_URI,
+    RESPONSE_TYPE
+} from '../config'
+import { api } from '../services/api';
+
 type User = {
     id: string;
     username: string;
@@ -17,6 +28,7 @@ type User = {
 
 type AuthContextData = {
     user: User;
+    signIn: () => Promise<void>;
 }
 
 type AuthProviderProps = {
@@ -27,10 +39,27 @@ export const AuthContext = createContext({} as AuthContextData);
 
 function AuthProvider({ children }: AuthProviderProps) {
     const [user, setUser] = useState<User>({} as User);
+    const [loading, setLoading] = useState(false);
+
+    async function signIn() {
+        try {
+            setLoading(true);
+
+            const authUrl = `${ api.defaults.baseURL }/oauth2/authorize?client_id=${ CLIENT_ID }&redirect_uri=${ REDIRECT_URI }&response_type=${ RESPONSE_TYPE }&scope=${ SCOPE }`;
+            console.log(authUrl)
+
+            AuthSession
+             .startAsync({ authUrl })
+
+        } catch (error) {
+
+        }
+    }
 
     return (
         <AuthContext.Provider value={{
-            user
+            user,
+            signIn
         }} >
             { children }
         </AuthContext.Provider>
